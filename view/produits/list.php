7be18388986989742c53995fe2e66abb6c998949
Kanto Randrianjatovo <46554479+kanto-fitiavana@users.noreply.github.com>
2020-09-02 18:49:39 +0300

    Update list.php

--- view/produits/list.php
+++ view/produits/list.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Produits</title>
+    <title>Upload Image</title>
 </head>
 <body>
     <form action="index.php?action=insert" method="POST" enctype="multipart/form-data">
@@ -12,13 +12,13 @@
         <input type="file" name="image" id="image">
         <input type="submit" name="submit">
     </form>
-    <h1>Liste des produits</h1>
+    <h1>Liste des images</h1>
     <?php 
-        foreach ($produits as $p) {
+        foreach ($images as $i) {
     ?>
-            <p><?= $p->id." ".$p->nom; ?></p>
-            <img src="././public/img/<?= $p->fichier; ?>" alt="Image">
-            <a href="?action=delete&id=<?= $p->id; ?>">Supprimer</a>
+            <p><?= $i->id." ".$i->nom; ?></p>
+            <img src="././public/img/<?= $i->fichier; ?>" alt="Image">
+            <a href="?action=delete&id=<?= $i->id; ?>">Supprimer</a>
     <?php
         }
     ?>
